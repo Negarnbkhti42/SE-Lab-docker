a31f9d4aa0c666ae5dd7f28635dedc22d50a77d7
Negarnbkhti42 <norob35@gmail.com>
2024-01-11 15:30:54 +0330

    Fix project structure

--- index.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-
-const PORT = process.env.PORT || 3000;
-const HOST_NAME = 'localhost';
-
-const app = express();
---
+++ post-service/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const PORT = process.env.PORT || 3002;
+
+const dbUsername = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD
+const dbPort = process.env.DB_PORT;
+
+const app = express();
+
+await mongoose.connect(`mongodb://${dbUsername}:${dbPassword}@mongo:${dbPort}`);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+});
